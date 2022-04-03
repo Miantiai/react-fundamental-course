02d65bc754c27940d4f16d1138a247b54b77fcaa
miantiai <miheevn100@gmail.com>
2022-04-03 18:23:14 +1000

    Create_form

--- src/App.js
+++ src/App.js
@@ -1,58 +1,43 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import PostList from './components/PostList/PostList';
-import './styles/App.css';
-import FormInput from './components/UI components/FormInput/FormInput.jsx';
-import FormButton from './components/UI components/FormButton/FormButton.jsx';
+import Button from './components/UI components/Button/Button';
+import Input from './components/UI components/Input/Input';
+import './styles/index.css';
 
-export default function App() {
+function App() {
   const [posts, setPosts] = useState([
-    {
-      id: 0,
-      title: 'title',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dicta distinctio beatae itaque nisi vel iste adipisci laborum et, ullam aliquam at eveniet nesciunt quo quis eum quibusdam praesentium sit nobis hic quam. Ea quas, aperiam vel voluptates voluptatibus commodi cumque ratione id cupiditate pariatur nesciunt deleniti veritatis impedit! Nostrum officiis ut aspernatur ex error expedita officia rem blanditiis voluptatibus. Quae, aut tenetur suscipit cum ratione expedita maxime molestias quis! Architecto odit ea consectetur pariatur quidem molestias aut debitis laudantium, magni quibusdam veritatis voluptas iusto error itaque, optio totam culpa animi velit blanditiis sunt cupiditate commodi ab ipsa! Ipsa, consectetur.'
-    },
+    { id: 1, title: 'JavaScrpt', body: 'description' },
+    { id: 2, title: 'JavaScrpt 2', body: 'description' },
+    { id: 3, title: 'JavaScrpt 3', body: 'description' },
   ]);
 
   const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('')
+  const [body, setBody] = useState('');
 
-  //function for add post in PostList
-  const addPost = (e) => {
+  const addNewPost = (e) => {
     e.preventDefault();
-    const newPost = {
-      id: Date.now(),
-      title,
-      description
-    }
-    setPosts([...posts, newPost])
-    setTitle('')
-    setDescription('')
-  }
-
-
-
-
-
+  };
   return (
-    <div className='App'>
-      <form className='FormAddPosts'>
-        <FormInput
+    <div className="App">
+      <form>
+        <Input
+          type="text"
           value={title}
-          placeholder='Name'
-          onChange={e => setTitle(e.target.value)}
-          tabIndex={1}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Post name"
         />
-        <FormInput
-          value={description}
-          placeholder='Description'
-          onChange={e => setDescription(e.target.value)}
-          tabIndex={2}
+        <Input
+          type="text"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          placeholder="Post description"
         />
-        <div>
-          <FormButton onClick={addPost} tabIndex={3}>Add post</FormButton></div>
+        <Button onClick={addNewPost} disablded="true">
+          Create post
+        </Button>
       </form>
-      <PostList posts={posts} title='Post List'/>
+      <PostList posts={posts} title={'Post list'} />
     </div>
-  )
+  );
 }
-    
+export default App;
